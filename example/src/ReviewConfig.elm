3a5a56fb7642846db348785dd18ebc60d634a2d9
Morten Kolstad <morten.kolstad@bekk.no>
2024-11-26 13:09:10 +0100

    .

--- example/src/ReviewConfig.elm
+++ example/src/ReviewConfig.elm
@@ -23,11 +23,5 @@ import Review.Rule exposing (Rule)
 
 config : List Rule
 config =
-    [ NoUnused.CustomTypeConstructors.rule []
-    , NoUnused.CustomTypeConstructorArgs.rule
-    , NoUnused.Dependencies.rule
-    , NoUnused.Exports.rule
-    , NoUnused.Parameters.rule
-    , NoUnused.Patterns.rule
-    , NoUnused.Variables.rule
+    [ 
     ]
